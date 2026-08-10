f4dd16bf6010835bcad0fab555af957326702594
Dery Wahyu Perdana <dery.5230411153@student.uty.ac.id>
2026-08-10 15:41:45 +0700

    Add notification routes to web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\InstitutionMembershipController;
+use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\OnboardingController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,21 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::post('institution-memberships', [InstitutionMembershipController::class, 'store'])
         ->middleware('throttle:institution-membership-request')
         ->name('institution-memberships.store');
+
+    Route::get('notifications', [NotificationController::class, 'index'])
+        ->name('notifications.index');
+
+    Route::post('notifications/{id}/read', [NotificationController::class, 'markRead'])
+        ->name('notifications.mark-read');
+
+    Route::post('notifications/read-all', [NotificationController::class, 'markAllRead'])
+        ->name('notifications.mark-all-read');
+
+    Route::get('notifications/{id}/navigate', [NotificationController::class, 'navigate'])
+        ->name('notifications.navigate');
+
+    Route::post('notification-preferences', [NotificationController::class, 'updatePreference'])
+        ->name('notification-preferences.update');
 });
 
 require __DIR__.'/settings.php';
