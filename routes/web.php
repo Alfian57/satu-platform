c8eabff22a0fd041d65b7d016d473f8b71e6b246
Dery Wahyu Perdana <dery.5230411153@student.uty.ac.id>
2026-08-10 15:50:37 +0700

    Add notification routes after main merge

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\InstitutionMembershipController;
+use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\OnboardingController;
 use App\Http\Controllers\TalentSearchController;
 use Illuminate\Support\Facades\Route;
@@ -22,6 +23,21 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     Route::get('recruiter/talent/candidates/{id}', [TalentSearchController::class, 'show'])
         ->name('recruiter.talent.candidates.show');
+
+    Route::get('notifications', [NotificationController::class, 'index'])
+        ->name('notifications.index');
+
+    Route::post('notifications/{id}/read', [NotificationController::class, 'markRead'])
+        ->name('notifications.mark-read');
+
+    Route::post('notifications/read-all', [NotificationController::class, 'markAllRead'])
+        ->name('notifications.mark-all-read');
+
+    Route::get('notifications/{id}/navigate', [NotificationController::class, 'navigate'])
+        ->name('notifications.navigate');
+
+    Route::post('notification-preferences', [NotificationController::class, 'updatePreference'])
+        ->name('notification-preferences.update');
 });
 
 require __DIR__.'/settings.php';
